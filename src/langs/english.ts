41f99bf31c62769fe2bbd6163462a7165d33252f
cl4pper <cl4pper@gmail.com>
2022-07-26 22:54:34 +0100

    feat: create langs file

---
+++ src/langs/english.ts
@@ -0,0 +1,11 @@
+import * as Types from './types';
+
+export const login: Types.Login = {
+  button: 'Sign in',
+  signup: 'Create a new account.',
+  header: 'Welcome to the Lowest League app.',
+  labels: {
+    username: 'Username',
+    password: 'Password',
+  }
+};
